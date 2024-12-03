9b4aaf0c0fa246f33747730155e24652be8a583b
matyaskomuves <mkomuves714@gmail.com>
2024-12-03 10:31:52 +0100

    Updated Validation Function, small changes in CSS

--- JS/script.js
+++ JS/script.js
@@ -32,28 +32,38 @@ document.querySelectorAll('.notInView').forEach(el => {
 function subscribeValidation() {
     const email = document.getElementById('email').value;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const allowedDomains = ['gmail.com', 'yahoo.com', 'outlook.com']
 
     if (!email) {
         alert('Please enter an email address');
         return;
     }
 
+    let formContainer = document.getElementById('formContainer');
+    const existingFailureMessage = document.querySelector('.not-subscribed');
+    const existingSuccessMessage = document.querySelector('.subscribed');
+
+    if (existingSuccessMessage) {
+        existingSuccessMessage.remove();
+    }
+    if (existingFailureMessage) {
+        existingFailureMessage.remove();
+    }
+
     if (emailRegex.test(email)) {
-        const existingSuccessMessage = document.querySelector('.container');
-        if (existingSuccessMessage) {
-            existingSuccessMessage.remove();
+        const domain = email.split('@')[1];
+        if (allowedDomains.includes(domain)) {
+            const divSuccess = document.createElement('div');
+            divSuccess.classList.add('container', 'subscribed');
+            divSuccess.innerHTML = '<p>Thank you for subscribing! :-]</p>';
+
+            formContainer.appendChild(divSuccess);
+        } else {
+            const divFailure = document.createElement('div');
+            divFailure.classList.add('container', 'not-subscribed');
+            divFailure.innerHTML = '<p>The given email address does not exist! :-[</p>';
+
+            formContainer.appendChild(divFailure);
         }
-
-        const divSuccess = document.createElement('div');
-        divSuccess.classList.add('container');
-        divSuccess.innerHTML = '<p>Thank you for subscribing!</p>';
-
-        document.body.appendChild(divSuccess);
-    } else {
-        const divFailure = document.createElement('div');
-        divFailure.classList.add('container');
-        divFailure.innerHTML = '<p>The given email does not exist!</p>';
-
-        document.body.appendChild(divFailure);
     }
 }
